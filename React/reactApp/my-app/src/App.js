35ac57be20906a3f8a6fe8da990b2654978f5702
H. Avni Cubukcu <huseyinac@gmail.com>
2022-02-02 20:57:20 +0300

    counter, texfield added

--- React/reactApp/my-app/src/App.js
+++ React/reactApp/my-app/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import PropTypes from 'prop-types';
+import React from "react";
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -15,6 +17,9 @@ function App() {
         <ShoppingCart date={Date()}/>
         <Person name={"qwe"} eyeColor={"qweq"}/>
         <Camper />
+        <ShoppingList/>
+        <Counter/>
+        <ControlledInput />
         <div id="mydiv"></div>
         <a
           className="App-link"
@@ -41,6 +46,22 @@ const ShoppingCart = (props) => {
 };
 
 ShoppingCart.defaultProps = {fx:"qwer"};
+
+class ShoppingList extends React.Component{
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
 
 
 const Person = (props)=> {  
@@ -75,9 +96,78 @@ Camper.propTypes = {
   name: PropTypes.string.isRequired
 };
 
+class Counter extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      count: 0
+    }
 
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
 
+  increment(){
+    this.setState((s)=>({
+      count: s.count+1
+    }));
+  }
 
+  decrement(){
+    if(this.state.count > 0){
+      this.setState(s => ({
+        count: s.count-1
+      }));
+    }      
+  }
+
+  reset(){
+    this.setState(()=>({
+      count: 0
+    }));
+  }
+
+  render(){
+    return(
+      <div>
+        <button className='inc' onClick={this.increment}>Inc</button>
+        <button className='dec' onClick={this.decrement} >Dec</button>
+        <button className='res' onClick = {this.reset}>Reset</button>
+        <h1>{this.state.count}</h1>
+      </div>
+    );
+  }
+
+}
+
+class ControlledInput extends React.Component{
+  constructor(props){
+    super(props);
+    this.state= {
+      input: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(event){
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <input value={this.state.input}  onChange= {this.handleChange} ></input>
+        <p> {this.state.input} </p>
+      </div>
+    )
+  }
+
+}
 
 
 
